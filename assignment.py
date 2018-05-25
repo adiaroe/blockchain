8742832b3448bf48c5bf2538215451efc985aa27
Adithya <adithya.alankara@gmail.com>
2018-05-25 10:23:53 -0400

    end of transactions

--- assignment.py
+++ assignment.py
@@ -1,12 +1,4 @@
-names = ["sarada", "adi", "ushe", "nagesh"]
-print(names)
-print('-'*50)
-for name in names:
-    if len(name) > 5 and ('n' in name or 'N' in name):
-        print(name)
-
-while(len(names) != 0):
-    names.pop()
-
-print(names)
-print('-'*50)
+person = {'name': 'Adithya','age': 22, 'hobbies':('gyming', 'coding', 'learning')}
+name_list = [name['name'] for name in person]
+more_20 = all[age['age']>20 for age in person]
+copy_name_list = name_list[:]
