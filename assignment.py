611df9d006e9ddb0c51e7c38f1c06381ccbebef5
Adithya <adithya.alankara@gmail.com>
2018-06-07 18:23:40 -0400

    06/07/18

--- assignment.py
+++ assignment.py
@@ -1,4 +1,4 @@
-person = {'name': 'Adithya','age': 22, 'hobbies':('gyming', 'coding', 'learning')}
-name_list = [name['name'] for name in person]
-more_20 = all[age['age']>20 for age in person]
-copy_name_list = name_list[:]
+def normal(another):
+    print(another())
+
+
--- blockchain.py
+++ blockchain.py
@@ -1,37 +1,43 @@
+import functools
+import hashlib
+import json
+
 MINING_REWARD = 10
 
-#creating a genesis block which denotes the starting block without any transactions
-genesis_block =  {
-    'previous_hash':'', 
-    'index':0, 
-    'transactions':[]
+# creating a genesis block which denotes the starting block without any transactions
+genesis_block = {
+    'previous_hash': '',
+    'index': 0,
+    'transactions': []
 }
-#initializing the current blockchain with the genesis block
+# initializing the current blockchain with the genesis block
 blockchain = [genesis_block]
-#store open transactions to evnetually mine a block
+# store open transactions to evnetually mine a block
 open_transactions = []
 owner = 'Adithya'
-#intializing the participant list with the owner's name
-participants =  {owner}
+# intializing the participant list with the owner's name
+participants = {owner}
 
 
 def get_balance(participant):
+    """Get the remaining balance of a particular participant
+
+    Arguments:
+        participant: The participant for whom we calculate the for
+    """
     tx_sender = [[tx['amount'] for tx in block['transactions']
                   if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx['amount']
                       for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0.0
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    #print(tx_sender)
+    amount_sent = functools.reduce(
+        lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
     tx_recipient = [[tx['amount'] for tx in block['transactions']
                      if tx['recipient'] == participant] for block in blockchain]
-    amount_received = 0.0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_received += tx[0]
-    return amount_received - amount_sent
+    amount_received = functools.reduce(
+        lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
+    return (amount_received - amount_sent)
 
 
 def get_last_blockchian_value():
@@ -41,11 +47,11 @@ def get_last_blockchian_value():
     return blockchain[-1]
 
 
-def add_transaction(recipient, sender = owner, amount = 1.0):
-    transaction =  {
-        'sender':sender, 
-        'recipient':recipient, 
-        'amount':amount
+def add_transaction(recipient, sender=owner, amount=1.0):
+    transaction = {
+        'sender': sender,
+        'recipient': recipient,
+        'amount': amount
     }
     if verify_transaction(transaction):
         open_transactions.append(transaction)
@@ -61,23 +67,24 @@ def verify_transaction(transaction):
 
 
 def hash_block(block):
-    return '-'.join([str(block[key])for key in block])
+    return hashlib.sha256(json.dumps(block).encode()).hexdigest()
 
 
 def mine_block():
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
-    reward_transaction =  {
-        'sender':'MINING', 
-        'recipient':owner, 
-        'amount':MINING_REWARD
+    print(hashed_block)
+    reward_transaction = {
+        'sender': 'MINING',
+        'recipient': owner,
+        'amount': MINING_REWARD
     }
     duplicate_open_transactions = open_transactions[:]
-    duplicate_open_transaction.append(reward_transaction)
-    block =  {
-        'previous_hash':hashed_block, 
-        'index':len(blockchain), 
-        'transactions':duplicate_open_transaction
+    duplicate_open_transactions.append(reward_transaction)
+    block = {
+        'previous_hash': hashed_block,
+        'index': len(blockchain),
+        'transactions': duplicate_open_transactions
     }
     blockchain.append(block)
     return True
@@ -122,17 +129,16 @@ while flag:
     print('5: Check transaction validity')
     print('h: Manipulate the chain')
     print('q: Quit')
-    
+
     user_choice = input('Your choice: ')
 
     if (user_choice == '1'):
         tx_info = get_transaction_info()
         recipient, amount = tx_info
-        if add_transaction(recipient, amount = amount):
+        if add_transaction(recipient, amount=amount):
             print("Added Transaction!")
         else:
             print("Transaction Failed!")
-        print(open_transactions)
     elif (user_choice == '2'):
         if mine_block():
             open_transactions = []
@@ -141,16 +147,16 @@ while flag:
     elif user_choice == '4':
         print(participants)
     elif user_choice == '5':
-        if (verify_transaction()):
+        if (verify_transactions()):
             print('All transactions are valid')
         else:
             print('There are invalid transactions')
     elif user_choice == 'h':
         if (len(blockchain) >= 1):
-            blockchain[0] =  {
-                'previous_hash':'', 
-                'index':0, 
-                'transactions':[ {'sender':'ushe', 'recipient':'Adithya', 'amount':10.0}]
+            blockchain[0] = {
+                'previous_hash': '',
+                'index': 0,
+                'transactions': [{'sender': 'ushe', 'recipient': 'Adithya', 'amount': 10.0}]
             }
     elif (user_choice == 'q'):
         flag = False
@@ -160,6 +166,6 @@ while flag:
         print_blockchain_elements()
         print('Invalid Blockchain')
         break
-    print(get_balance('Adithya'))
+    print(f"Balance of {owner}: {get_balance(owner):6.2f}")
 else:
     print('User Aborted!')
